7bed1326c4cd47237deedc5b78b83e77990c7615
shun9 <hanashun9@gmail.com>
2017-09-11 11:46:11 +0900

    追跡するモンスターが待機中に少しだけ移動する処理を作成 Make a process that monster tracking moves slightly while waiting

--- IGCC2017_TeamA/Assets/Scripts/Monster/ChaseMonster/State/ChaseMonsterIdleState.cs
+++ IGCC2017_TeamA/Assets/Scripts/Monster/ChaseMonster/State/ChaseMonsterIdleState.cs
@@ -2,7 +2,7 @@
 //* @file  :ChaseMonsterIdleState.cs
 //* @brief :ロボットを追跡するモンスター待機状態
 //* @brief :State to idle the robot
-//* @date  :2017/09/07
+//* @date  :2017/09/11
 //* @author:S.Katou
 //************************************************/
 using System.Collections;
@@ -17,6 +17,11 @@ public class ChaseMonsterIdleState : State<ChaseMonster>
 	//Target to chase
 	private GameObject m_target = null;
 
+	private float m_timer = 0.0f;
+	private Vector3 m_spd;
+	private MonsterStats m_stats = null;
+	private Collider2D m_area = null;
+
 	/// <summary>
 	/// 開始処理
 	/// </summary>
@@ -24,12 +29,32 @@ public class ChaseMonsterIdleState : State<ChaseMonster>
 	{
 		m_target = null;
 		obj.m_anime.SetBool("isIdle", true);
+
+		if (m_stats == null)
+		{
+			m_stats = obj.GetStats();
+		}
+
+		if (m_area == null)
+		{
+			m_area = obj.GetStats().m_chargeArea.GetComponent<Collider2D>();
+		}
 	}
 
 	/// <summary>
 	/// 実行処理
 	public override void Execute()
 	{
+		m_timer += Time.deltaTime;
+
+		obj.transform.position += m_spd;
+		ClampPos();
+		if (m_timer > 1.0f)
+		{
+			m_timer = 0.0f;
+			SetSpd();
+		}
+
 		if (m_target == null)
 		{
 			m_target = obj.GetStats().m_robotList.GetTarget(obj.transform.position);
@@ -47,5 +72,43 @@ public class ChaseMonsterIdleState : State<ChaseMonster>
 	public override void Exit()
 	{
 		obj.m_anime.SetBool("isIdle", false);
+		obj.m_anime.SetBool("isWalked", false);
+	}
+
+
+	private void SetSpd()
+	{
+		if (Random.Range(0, 19) < 6)//60% to not move
+		{
+			m_spd = Vector3.zero;
+
+			obj.m_anime.SetBool("isIdle", true);
+			obj.m_anime.SetBool("isWalked", false);
+
+		}
+		else
+		{
+			m_spd = new Vector3(Random.Range(-0.3f, 0.3f), Random.Range(-0.3f, 0.3f), 0.0f) * m_stats.SPD * Time.deltaTime;
+			obj.SpriteFlipX(obj.transform.position.x + m_spd.x);
+
+			obj.m_anime.SetBool("isIdle", false);
+			obj.m_anime.SetBool("isWalked", true);
+		}
+	}
+
+
+	private void ClampPos()
+	{
+		Vector3 objPos = obj.transform.position;
+		Vector3 areaPos = obj.GetStats().m_chargeArea.transform.position;
+		var size = m_area.bounds.size;
+		float sizeX = size.x / 2.0f;
+		float sizeY = size.y / 2.0f;
+
+		objPos.x = Mathf.Clamp(objPos.x, areaPos.x - sizeX, areaPos.x + sizeX);
+		objPos.y = Mathf.Clamp(objPos.y, areaPos.y - sizeY, areaPos.y + sizeY);
+
+
+		obj.transform.position = objPos;
 	}
 }
