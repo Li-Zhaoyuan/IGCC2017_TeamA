272d9bb2151bcbb22d1d994747b153706bec3f2e
shun9 <hanashun9@gmail.com>
2017-09-11 17:38:40 +0900

    簡易的なシーン遷移を作成 シーンが切り替わる時にフェードする処理を作成 Create a simple scene transition Create a process to fade when the scene changes

---
+++ IGCC2017_TeamA/Assets/Scripts/Scene/ChangeScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+
+	SceneInstructor m_sceneInstructor = null;
+
+	// Use this for initialization
+	void Start () {
+		m_sceneInstructor = SceneInstructor.instance;
+	}
+
+
+	public void LoadTitle()
+	{
+		m_sceneInstructor.LoadMainScene(GameScene.Title);
+	}
+	public void LoadMenu()
+	{
+		m_sceneInstructor.LoadMainScene(GameScene.Menu);
+	}
+	public void LoadPlay()
+	{
+		m_sceneInstructor.LoadMainScene(GameScene.Play);
+	}
+}
