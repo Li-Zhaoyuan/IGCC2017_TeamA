e9a8e09fcce9eb78ab9c642a4d500e667aeaf84d
shun9 <hanashun9@gmail.com>
2017-09-12 11:11:00 +0900

    シーンが変わったときにロボットの情報を保持しておく シーンが変わったときにオブジェクトを破棄 Keep robot information when the scene changes Discard objects when the scene changes

---
+++ IGCC2017_TeamA/Assets/Scripts/Scene/ItemHolder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum ITEM_LIST
+{
+	ITEM = 0,
+}
+public class ItemHolder : MonoBehaviour
+{
+
+	static public ItemHolder instance;
+
+	Dictionary<ITEM_LIST, int> m_itemMap;
+
+	void Awake()
+	{
+		if (instance == null)
+		{
+			m_itemMap = new Dictionary<ITEM_LIST, int>() {
+				{ITEM_LIST.ITEM,0 },
+			};
+
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	bool UseItem(ITEM_LIST item,int num)
+	{
+		int remainNum = m_itemMap[item];
+
+		if (remainNum > num)
+		{
+			m_itemMap[item] = remainNum - num;
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ IGCC2017_TeamA/Assets/Scripts/Scene/Test/TestRobotSummon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRobotSummon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var list = RobotHolder.instance.robotList;
+
+		foreach(var robot in list)
+		{
+			if (robot != null)
+			{
+				robot.SetActive(true);
+				robot.transform.position = Vector3.zero;
+			}
+		}
+	}
+
+	void OnDestroy()
+	{
+		var list = RobotHolder.instance.robotList;
+
+		foreach (var robot in list)
+		{
+			if (robot != null)
+			{
+				robot.SetActive(false);
+			}
+		}
+	}
+}
