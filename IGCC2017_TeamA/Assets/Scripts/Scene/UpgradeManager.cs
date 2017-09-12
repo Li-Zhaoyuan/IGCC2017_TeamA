72f29c090418d47789086e972b552d1c28ae6b4c
shun9 <hanashun9@gmail.com>
2017-09-12 22:29:58 +0900

    ロボットのアップグレードを作成 Create robot upgrade

--- IGCC2017_TeamA/Assets/Scripts/Scene/UpgradeManager.cs
+++ IGCC2017_TeamA/Assets/Scripts/Scene/UpgradeManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class UpgradeManager : MonoBehaviour {
 
+	[SerializeField]
+	private ClickObject m_clickObject;
+
+	private GameObject m_target = null;
+
 	private ItemHolder m_item = null;
 
 	private ITEM_TYPE m_type = ITEM_TYPE.RESOURCE_COG;
@@ -12,81 +17,86 @@ public class UpgradeManager : MonoBehaviour {
 	void Start () {
 		m_item = ItemHolder.instance;
 	}
-
-	public void UpgradeAttack(GameObject robot)
+	void Update()
 	{
-		var stats = robot.GetComponent<Robot_Status>().base_attack_point;
+		m_target = m_clickObject.m_target;
+	}
+
+	public void UpgradeAttack()
+	{
+		var stats = m_target.GetComponent<Robot_Status>().base_attack_point;
+
+		bool isUpgrade = m_item.UseItem(m_type,(int)stats);
+		isUpgrade = m_item.UseItem(m_type, 0);
+		if (isUpgrade)
+		{
+			m_target.GetComponent<Robot_Status>().AddBaseAttackPoint(1.0f);
+		}
+	}
+
+	public void UpgradeDefence()
+	{
+		var stats = m_target.GetComponent<Robot_Status>().base_defence_point;
 
 		bool isUpgrade = m_item.UseItem(m_type,(int)stats);
 		if (isUpgrade)
 		{
-			robot.GetComponent<Robot_Status>().AddBaseAttackPoint(1.0f);
+			m_target.GetComponent<Robot_Status>().AddBaseDefencePoint(1.0f);
 		}
 	}
 
-	public void UpgradeDefence(GameObject robot)
+	public void UpgradeSpeed()
 	{
-		var stats = robot.GetComponent<Robot_Status>().base_defence_point;
+		var stats = m_target.GetComponent<Robot_Status>().base_speed_point;
 
 		bool isUpgrade = m_item.UseItem(m_type,(int)stats);
 		if (isUpgrade)
 		{
-			robot.GetComponent<Robot_Status>().AddBaseDefencePoint(1.0f);
+			m_target.GetComponent<Robot_Status>().AddBaseSpeedPoint(0.1f);
 		}
 	}
 
-	public void UpgradeSpeed(GameObject robot)
+	public void UpgradeMagic()
 	{
-		var stats = robot.GetComponent<Robot_Status>().base_speed_point;
-
-		bool isUpgrade = m_item.UseItem(m_type,(int)stats);
-		if (isUpgrade)
-		{
-			robot.GetComponent<Robot_Status>().AddBaseSpeedPoint(0.1f);
-		}
-	}
-
-	public void UpgradeMagic(GameObject robot)
-	{
-		var stats = robot.GetComponent<Robot_Status>().base_magic_point;
+		var stats = m_target.GetComponent<Robot_Status>().base_magic_point;
 
 		bool isUpgrade = m_item.UseItem(m_type, (int)stats);
 		if (isUpgrade)
 		{
-			robot.GetComponent<Robot_Status>().AddBaseMagicPoint(1.0f);
+			m_target.GetComponent<Robot_Status>().AddBaseMagicPoint(1.0f);
 		}
 	}
 
-	public void UpgradeLuck(GameObject robot)
+	public void UpgradeLuck()
 	{
-		var stats = robot.GetComponent<Robot_Status>().base_luck_point;
+		var stats = m_target.GetComponent<Robot_Status>().base_luck_point;
 
 		bool isUpgrade = m_item.UseItem(m_type,(int)stats);
 		if (isUpgrade)
 		{
-			robot.GetComponent<Robot_Status>().AddLuckPoint(1.0f);
+			m_target.GetComponent<Robot_Status>().AddLuckPoint(1.0f);
 		}
 	}
 
-	public void UpgradeHP(GameObject robot)
+	public void UpgradeHP()
 	{
-		var stats = robot.GetComponent<Robot_Status>().base_health_point;
+		var stats = m_target.GetComponent<Robot_Status>().base_health_point;
 
 		bool isUpgrade = m_item.UseItem(m_type,(int)stats);
 		if (isUpgrade)
 		{
-			robot.GetComponent<Robot_Status>().AddBaseHealthPoint(10.0f);
+			m_target.GetComponent<Robot_Status>().AddBaseHealthPoint(10.0f);
 		}
 	}
 
-	public void UpgradeEnergy(GameObject robot)
+	public void UpgradeEnergy()
 	{
-		var stats = robot.GetComponent<Robot_Status>().base_energy_point;
+		var stats = m_target.GetComponent<Robot_Status>().base_energy_point;
 
 		bool isUpgrade = m_item.UseItem(m_type,(int)stats);
 		if (isUpgrade)
 		{
-			robot.GetComponent<Robot_Status>().AddBaseEnergyPoint(10.0f);
+			m_target.GetComponent<Robot_Status>().AddBaseEnergyPoint(10.0f);
 		}
 	}
 }
