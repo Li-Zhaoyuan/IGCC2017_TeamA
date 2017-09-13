dbbdb351406a4adb1db8a351b29768c47428db59
shun9 <hanashun9@gmail.com>
2017-09-13 22:13:31 +0900

    エフェクトが消えるように修正 敵の出現時に攻撃SEが鳴る不具合を修正 エリア内に出現する敵の数を制限 Fix to disappear the effect Fixed a bug that Attack SE sounded when enemies appeared Restrict the number of enemies appearing in the area

---
+++ IGCC2017_TeamA/Assets/Scripts/Monster/Area/MonsterList.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterList : MonoBehaviour {
+
+	private List<GameObject> m_monsterList = new List<GameObject>();
+
+	[SerializeField]
+	private int m_maxMonster = 10;
+
+	public int MaxMonster
+	{
+		get { return m_maxMonster; }
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (m_monsterList.Contains(null))
+		{
+			m_monsterList.Remove(null);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Monster")
+		{
+			m_monsterList.Add(col.gameObject);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Monster")
+		{
+			m_monsterList.Remove(col.gameObject);
+		}
+	}
+
+	public int GetListLength()
+	{
+		return m_monsterList.Count;
+	}
+}
