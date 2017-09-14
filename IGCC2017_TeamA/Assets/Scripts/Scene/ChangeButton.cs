e3d25561a32962d73a4444b7b1e8207defd66e28
shun9 <hanashun9@gmail.com>
2017-09-14 15:39:57 +0900

    タイトル作成 いくつかのバグを修正 Title creation Fixed some bugs

---
+++ IGCC2017_TeamA/Assets/Scripts/Scene/ChangeButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeButton : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject m_buttonImage;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+
+		}
+		else
+		{
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D collition2d = Physics2D.OverlapPoint(mousePos);
+			if (collition2d)
+			{
+				m_buttonImage.SetActive(true);
+			}
+			else
+			{
+				m_buttonImage.SetActive(false);
+			}
+		}
+	}
+}
